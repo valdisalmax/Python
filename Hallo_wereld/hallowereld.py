7c8ece9e8ca9fddccd96c8cce236e3850f751453
valdisalmax <b.lodder@outlook.com>
2020-08-12 17:54:02 +0200

    Delete hallowereld.py

--- Hallo_wereld/hallowereld.py
+++
@@ -1,6 +0,0 @@
-print('Hallo wereld!')
-print('het begin is meestal niet makkelijk')
-naam = input('je naam aub. ')
-print('aaah jij bent ',naam)
-achteruit =''.join(reversed(naam))
-print('Ooow achterstevoren heet je dan ',achteruit)
